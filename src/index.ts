4c0b1e92833448cd817b46a91d9826ea189b848f
Burner <burner@burner.gay>
2022-07-04 10:56:02 -0500

    Rewrote for wrangler 2

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import app from './app'
 
 export interface Env {
   // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
@@ -23,8 +15,8 @@ export default {
   async fetch(
     request: Request,
     env: Env,
-    ctx: ExecutionContext
+    ctx: ExecutionContext,
   ): Promise<Response> {
-    return new Response("Hello World!");
+    return app.fetch(request, env, ctx)
   },
-};
+}
